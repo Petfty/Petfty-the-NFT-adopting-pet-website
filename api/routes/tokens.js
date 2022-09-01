9d0c60eac3bbd5b6a1c07ea60240ea2e73790f9e
aLVINlEE9 <fldh3746@gmail.com>
2022-09-01 15:02:35 +0900

    feat burn and get request

--- api/routes/tokens.js
+++ api/routes/tokens.js
@@ -17,7 +17,7 @@ router.get("/createTokenURL", async (req, res) => {
 		const functionJson = '{ "constant": false, "inputs": [ { "name": "to", "type": "address" }, { "name": "tokenId", "type": "uint256" }, { "name": "tokenURI", "type": "string" } ], "name": "mintWithTokenURI", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }';
 		const request_key = await API.executeContract(process.env.NFT_CONTRACT_ADDRESS, functionJson, "0", `["${req.body.toAddress}", "${req.body.tokenId}", "${metadataURL}"]`);
 		if (!request_key) {
-			res.status(404).json("failed to execute contract");
+			res.status(404).json("failed to execute create token contract");
 		}
 
 		// response to client
@@ -86,7 +86,7 @@ router.get("/purchaseTokenURL", async (req, res) => {
 		const functionJson = '{ "constant": false, "inputs": [ { "name": "tokenId", "type": "uint256" }, { "name": "NFTAddress", "type": "address" } ], "name": "buyNFT", "outputs": [ { "name": "", "type": "bool" } ], "payable": true, "stateMutability": "payable", "type": "function" }';
 		const request_key = await API.executeContract(process.env.MARKET_CONTRACT_ADDRESS, functionJson, "10000000000000000", `["${req.body.tokenId}", "${process.env.NFT_CONTRACT_ADDRESS}"]`);
 		if (!request_key) {
-			res.status(404).json("failed to execute contract");
+			res.status(404).json("failed to execute purchase token contract");
 		}
 
 		// response to client
@@ -115,9 +115,30 @@ router.put("/purchaseToken", async (req, res) => {
 	}
 });
 
+// get burn a Token URL
+router.get("/burnTokenURL", async (res, req) => {
+	try {
+		// execute contract
+		const functionJson = '{ "constant": false, "inputs": [ { "name": "from", "type": "address" }, { "name": "to", "type": "address" }, { "name": "tokenId", "type": "uint256" } ], "name": "safeTransferFrom", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }';
+		const request_key = await API.executeContract(process.env.NFT_CONTRACT_ADDRESS, functionJson, "0", `["${"0x0000000000000000000000000000000000000000"}", "${process.env.MARKET_CONTRACT_ADDRESS}", "${req.body.tokenId}"]`);
+		if (!request_key) {
+			res.status(403).json("failed to execute burn token contract")
+		}
+
+		// response to client
+		if (req.body.isMobile === true) {
+			res.status(200).json({url: process.env.MB_URL, request_key: request_key});
+		} else {
+			res.status(200).json({url: process.env.QR_URL, request_key: request_key});
+		}
+	} catch (err) {
+		req.status(500).json(err);
+	}
+})
+
 
 // delete a Token
-router.delete("/:id", async (req, res) => {
+router.delete("/burnToken/:id", async (req, res) => {
 	try {
 		const currentToken = await Token.findById(req.params.id);
 		if (currentToken.ownerId === req.body.ownerId) {
@@ -132,30 +153,34 @@ router.delete("/:id", async (req, res) => {
 })
 
 // get all Token on Market
-router.get("/all", async (req, res) => {
+router.get("/info/all", async (req, res) => {
 	try {
 		const nfts = await API.getNFTs(process.env.MARKET_CONTRACT_ADDRESS);
-		const tokens = await Token.find({});
-		res.status(200).json(tokens);
+		res.status(200).json(nfts);
 	} catch (err) {
 		res.status(500).json(err);
 	}
 })
 
-// get a Token on Market
-router.get("/:id", async (req, res) => {
+// get user's Token on Market
+router.get("/info/:userid", async (req, res) => {
 	try {
-		const currentToken = await Token.findById(req.params.id);
-		res.status(200).json(currentToken);
+		const currentUser = await User.findById(req.params.userid);
+		if (currentUser.walletAddress == req.body.walletAddress) {
+			const nfts = API.getNFTs(currentUser.walletAddress);
+			res.status(200).json(nfts);
+		} else {
+			res.status(403).json("you can delete only your Token");
+		}
 	} catch (err) {
 		res.status(500).json(err);
 	}
 })
 
-// get user's all Token
-router.get("/profile/:id", async (req, res) => {
+// get a token
+router.get("/info/:id", async (req, res) => {
 	try {
-		const user = await User.findOne({ _id: req.params.id })
+		const token = await Token.findOne({ _id: req.params.id })
 		res.status(200).json(user)
 	} catch (err) {
 		res.status(500).json(err);
